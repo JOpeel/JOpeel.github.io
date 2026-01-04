d853575674f85f0a0f0f9cc46a131a7d86105163
JOpeel <j.o.peel2@newcastle.ac.uk>
2026-01-04 13:43:20 +0000

    Making left bar not reload text fade on page navigation

--- Left.js
+++ Left.js
@@ -1,10 +1,15 @@
+
 class LeftComponent extends HTMLElement {
+
     connectedCallback() {
-        this.innerHTML = `            
+        const hasLoadedBefore = sessionStorage.getItem('loaded') === 'true';
+
+        if(!hasLoadedBefore){
+            this.innerHTML = `            
           <div id="left">  <!--            TODO dotted line -->
             <div id="topline">
 <!--                <a href="/index.html">-->
-                <img src="\\wordmark.svg" class="wordmark" alt="Jacob Peel">
+                <img src="/wordmark.svg" class="wordmark" alt="Jacob Peel">
 <!--                </a>-->
                 <div id="links">
                     <a href="/Documents/CV%20Jacob%20Peel.docx"><p> CV </p></a>
@@ -43,6 +48,58 @@ class LeftComponent extends HTMLElement {
     
         </div>
     `;
+            sessionStorage.setItem('loaded', 'true');
+
+        } else {
+            this.innerHTML = `            
+          <div id="left">  <!--            TODO dotted line -->
+            <div id="topline">
+<!--                <a href="/index.html">-->
+                <img src="/wordmark.svg" class="wordmark" alt="Jacob Peel">
+<!--                </a>-->
+                <div id="links">
+                    <a href="/Documents/CV%20Jacob%20Peel.docx"><p> CV </p></a>
+                    <a href="https://www.linkedin.com/in/jacob-peel-6408382ba"><p> LinkedIn </p></a>
+                </div>
+            </div>
+    
+            <div id="qualificationsstatic">
+                <div class="qualificationsline">
+                    Human-Computer Interaction MSc (2025)
+                </div>
+                <div class="qualificationsline">
+                    Computer Science BSc (2024)
+                </div>
+            </div>
+    
+            <div id="bio">
+                <p class="biolinestatic">
+                    UK-based Designer and engineer.
+                </p>
+                <p class="biolinestatic">
+                    Human-centred tools - ideation, research, prototyping, development.
+                </p>
+                <p class="biolinestatic">
+                    Visual identity and content design.
+                </p>
+                <p class="biolinestatic">
+                    This website showcases many of my projects as a portfolio, detailing the processes undertaken, problems solved, and skills utilised!
+                </p>
+                <p class="biolinestatic">
+                    When I’m not at my desk you can often find me up a mountain in disappointing conditions.
+                    <br>
+                    When I AM at my desk, you can find me at jacobowainpeel@gmail.com.
+                </p>
+            </div>
+    
+        </div>
+    `;
+            sessionStorage.setItem('loaded', 'true');
+        }
     }
+
+
+
 }
+
 customElements.define('my-left', LeftComponent);

235f0d4cc673a97a18d52e92aaf4b7ba41ca54f9
JOpeel <j.o.peel2@newcastle.ac.uk>
2026-01-05 22:35:13 +0000

    Optimising left bar reload behaviour and fixing project rotation appearance randomisation per session

--- randomiseprojects.js
+++ randomiseprojects.js
@@ -9,13 +9,36 @@ function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
-projects.forEach(project => {
+const rotationsArray = [];
+const marginsArray = [];
+let i = 0;
 
-    const rotation = getRandomValue(-4,4)
+if(sessionStorage.getItem('randomised') === 'true'){
+    i=0
+    projects.forEach(project => {
+        project.style.setProperty('--random-rotation', `${sessionStorage.getItem(i)}deg`);
+        i+=1
+        project.style.margin = `${sessionStorage.getItem(i)}px`;
+        i+=1
+    })
+} else {
+    i=0
+    projects.forEach(project => {
 
-    const margin = getRandomValue(convertRemToPixels(1), convertRemToPixels(3))
+        const rotation = getRandomValue(-4,4)
+        sessionStorage.setItem(i, rotation);
 
-    project.style.setProperty('--random-rotation', `${rotation}deg`);
-    project.style.margin = `${margin}px`;
+        i+=1
 
-})
+        const margin = getRandomValue(convertRemToPixels(1), convertRemToPixels(3))
+        sessionStorage.setItem(i, margin);
+
+        project.style.setProperty('--random-rotation', `${rotation}deg`);
+        project.style.margin = `${margin}px`;
+
+        i+=1;
+
+    })
+    sessionStorage.setItem('randomised', 'true');
+}
+
